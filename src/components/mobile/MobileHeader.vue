0c2b2f7791c67882831ca4a7296daf7eadd81eab
Mini-Sylar <minisylar3@gmail.com>
2022-11-10 22:24:06 +0000

    Added mobile header

---
+++ src/components/mobile/MobileHeader.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <nav>
+            <div class="logo">
+                <img src="@/assets/images/logo.png" alt="">
+            </div>
+            <div class="hamburger"></div>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
