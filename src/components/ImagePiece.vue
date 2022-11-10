f8b517da639cea86e1ec21bb7bed1c8dd19e2c20
Mini-Sylar <minisylar3@gmail.com>
2022-11-10 09:01:38 +0000

    Added images

--- src/components/ImagePiece.vue
+++ src/components/ImagePiece.vue
@@ -1,15 +1,26 @@
 <template lang="">
   <div>
-    
+    <div class="image-1">
+        <img :src="images[0]" alt="">
+    </div>
+    <div class="image-2">
+        <img :src="images[1]" alt="">
+    </div>
+    <div class="image-3">
+        <img :src="images[2]" alt="">
+    </div>
   </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            images: ['image-1', 'image-2', 'image-3']
+            images: ['./assets/images/subject-1', './assets/images/subject-2', './assets/images/subject-3']
         };
     },
 }
 </script>
-<style lang="" scoped src="@/assets/css/ImagesLanding.css;"></style>
+
+<style src="@/assets/css/ImagesLanding.css">
+
+</style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -9,11 +9,14 @@
       </p>
       <button type="button" class="request-demo-button">Request a Demo</button>
     </div>
-    <div class="right-side"></div>
+    <div class="right-side">
+        <app-image-piece></app-image-piece>
+    </div>
   </div>
 </template>
 
 <script>
+import ImagePiece from './ImagePiece.vue';
 export default {
   data() {
     return {
@@ -21,6 +24,9 @@ export default {
       catchBody:
         "Greenlfy is the leader in advocacy marketing. The Greenfly software platfom enables organizations of all sizes to harness the power of digital media to build awareness, increase loyalty, and drive revenue in our new digital-first world.",
     };
+  },
+  components: {
+    "app-image-piece": ImagePiece,
   },
 };
 </script>
