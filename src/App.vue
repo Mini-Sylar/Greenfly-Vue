16b91160ea8eb2152a80d81319ebf707fcd98f57
Mini-Sylar <minisylar3@gmail.com>
2022-11-10 12:37:50 +0000

    loaded data from parent instead

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template lang="">
   <div>
     <app-header></app-header>
-    <section>
-      <div class="is-a-container">
-         <app-landing></app-landing>
-      </div>
-    </section>
-    <section>
-      <div class="is-a-container">
-        <section>
-                 <app-seewhy></app-seewhy>
-        </section>
- 
-      </div>
-    </section>
+    <main>
+      <section>
+        <div class="is-a-container">
+          <app-landing></app-landing>
+        </div>
+      </section>
+      <section>
+        <div class="is-a-container">
+          <app-seewhy></app-seewhy>
+        </div>
+      </section>
+      <section>
+        <div class="is-a-container">
+           <app-sections v-bind:section_content='section_content.content_1'>
+           </app-sections>
+            <app-sections v-bind:section_content='section_content.content_2'>
+           </app-sections>
+        </div>
+       
+      </section>
+    </main>
   </div>
 </template>
 
@@ -21,12 +29,30 @@
 import Header from "./components/Header.vue";
 import Landing from "./components/Landing.vue";
 import SeeWhy from "./components/SeeWhy.vue";
+import Sections from './components/Sections.vue';
 export default {
-  name: "app",
+  name: "Greenfly",
+  data() {
+    return {
+      title: "Title",
+      body: "Body",
+      section_content: {
+        content_1: {
+          title: "Section 1",
+          body: "Body section that is here",
+        },
+        content_2: {
+          title: "Section 2",
+          body: "Body section that is here",
+        },
+      },
+    };
+  },
   components: {
     "app-header": Header,
     "app-landing": Landing,
     "app-seewhy": SeeWhy,
+    "app-sections": Sections,
   },
 };
 </script>
---
+++ src/components/Sections.vue
@@ -0,0 +1,37 @@
+<template lang="">
+  <div>
+    <div>
+        <div class="left-side">
+           <h2>{{section_content.title}}</h2>
+        </div>    
+        <div class="right-side">
+            Image Here
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        changeTitle: function (params) {
+
+        }
+    },
+    props: {
+        section_content: {
+            type: Object,
+            // required: true
+        }
+    },
+    // created: function (whichcontent) {
+    //     this.title = this.section_content.whichcontent.title;
+    //     this.body = this.section_content.whichcontent.body;
+    // },
+};
+</script>
+<style lang=""></style>
